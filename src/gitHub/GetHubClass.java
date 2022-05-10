4297cc1c4b05dbfabadf4c90014dcdcaae110076
Mohibullah Hamrah <mohib.hamrah@gmail.com>
2022-05-09 21:39:10 -0700

    changes after break

--- src/gitHub/GetHubClass.java
+++ src/gitHub/GetHubClass.java
@@ -2,4 +2,19 @@ package gitHub;
 
 public class GetHubClass {
 
+	public static void main(String[]args) {
+		
+		int a = 10;
+		int b =30;
+		int d = 10;
+		int c= a+b+d;
+		System.out.println(c);
+		
+		
+		
+	}
+	
+	
+	
+	
 }
